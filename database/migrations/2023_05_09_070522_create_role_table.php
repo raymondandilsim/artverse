f6e52a06fa002eac965c97120774927674c5c529
Raymond Andilsim <raymondandilsim@gmail.com>
2023-05-09 14:06:27 +0700

    peran -> role (model, seeder, migration)

--- database/migrations/2023_05_09_070522_create_role_table.php
+++ database/migrations/2023_05_09_070522_create_role_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('peran', function (Blueprint $table) {
+        Schema::create('role', function (Blueprint $table) {
             $table->id();
-            $table->string('nama_peran');
+            $table->string('nama_role');
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('peran');
+        Schema::dropIfExists('role');
     }
 };
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('role')->insert([
+            ['nama_role'=>'Admin'],
+            ['nama_role'=>'Member'],
+            ['nama_role'=>'Seniman'],
+        ]);
+    }
+}
