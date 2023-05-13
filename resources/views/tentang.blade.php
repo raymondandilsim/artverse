ae92b38cd7389c5564719fa3222b6009bc5e2ea3
Steven633 <steven.valentino49@gmail.com>
2023-05-13 15:52:30 +0700

    Page Tentang

---
+++ resources/views/tentang.blade.php
@@ -0,0 +1,15 @@
+@extends('Master')
+
+@section('Page-Title')
+    Tentang
+@endsection
+
+@section('Page-Contents')
+    <div class="bg-home d-flex align-items-center justify-content-center">
+        <h3>Tentang Kami</h3>
+        <h5>Bergabunglah, GRATIS!
+Platform terbuka untuk pecinta seni, seniman, galeri, kolektor, universitas, dan siapa saja yang terlibat dalam dunia seni.
+ 
+Buat profil Anda sendiri, buat koneksi, pengikut, posting Anda dan banyak lagi.</h5>
+    </div>
+@endsection
