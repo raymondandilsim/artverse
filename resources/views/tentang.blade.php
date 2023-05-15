98de73bab2fa8a66b8e1a1eb0cf2e23a27e4b557
Steven633 <steven.valentino49@gmail.com>
2023-05-15 14:54:10 +0700

    perbaikan tentang

--- resources/views/tentang.blade.php
+++ resources/views/tentang.blade.php
@@ -5,11 +5,13 @@
 @endsection
 
 @section('Page-Contents')
-    <div class="bg-home d-flex align-items-center justify-content-center">
+    <div class="p-5 d-flex justify-content-center">
         <h3>Tentang Kami</h3>
-        <h5>Bergabunglah, GRATIS!
+    </div>
+        <div class="p-5 text-start">
+            <p class="h5 lh-sm">Bergabunglah, GRATIS!
 Platform terbuka untuk pecinta seni, seniman, galeri, kolektor, universitas, dan siapa saja yang terlibat dalam dunia seni.
- 
-Buat profil Anda sendiri, buat koneksi, pengikut, posting Anda dan banyak lagi.</h5>
+Buat profil Anda sendiri, buat koneksi, pengikut, posting Anda dan banyak lagi.</p>
+            <p class="h5 lh-sm">Aplikasi yang akan kami kembangkan bernama "Artverse". Artverse adalah platform e-commerce berbasis website yang memudahkan para seniman untuk melakukan transaksi jual beli dengan konsumen-konsumen yang ada. Tujuan dari pembuatan ini supaya seniman diluar sana mendapatkan lapangan pekerjaan secara mudah hanya dengan melalui pameran yang berbasis online. Aplikasi ini memiliki manfaat untuk meningkatkan lagi pekerjaan seorang seniman supaya dapat lebih luas dikenal oleh banyak orang melalui karya mereka.</p>
     </div>
 @endsection
